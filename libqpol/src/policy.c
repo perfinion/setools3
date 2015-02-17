0e2688b51e0d6d1db9c81009f680e1a6af3abcc6
Jason Zaman <jason@perfinion.com>
2015-02-18 03:07:30 +0800

    libqpol: fix implicit include for fstat

--- libqpol/src/policy.c
+++ libqpol/src/policy.c
@@ -36,6 +36,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <asm/types.h>
 
 #include <sepol/debug.h>
